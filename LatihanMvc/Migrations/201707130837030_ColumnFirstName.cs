2318abf0fbf7d046f1d470aa9f5b46f8c61b93a0
Barkah Martiyana Ramdani <barkahmr@gmail.com>
2017-07-13 17:01:27 +0700

    Add project files.

---
+++ LatihanMvc/Migrations/201707130837030_ColumnFirstName.cs
@@ -0,0 +1,16 @@
+namespace LatihanMvc.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ColumnFirstName : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
